62c3d3cf297e1bb4f42ed273e84e8128c10003d7
Reimnop <rayngu2005@gmail.com>
2024-05-17 14:48:56 +0700

    refactor: reduce nesting

--- src/main/java/com/crimsoncrips/borninconfiguration/mixins/StalkerImmunity.java
+++ src/main/java/com/crimsoncrips/borninconfiguration/mixins/StalkerImmunity.java
@@ -9,6 +9,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import software.bernie.geckolib.animatable.GeoEntity;
@@ -17,47 +18,41 @@ import software.bernie.geckolib.core.animation.AnimatableManager;
 
 @Mixin(NightmareStalkerEntity.class)
 
-public class StalkerImmunity extends Monster implements GeoEntity {
+public abstract class StalkerImmunity extends Monster implements GeoEntity {
 
     protected StalkerImmunity(EntityType<? extends Monster> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
     @Override
-    public boolean hurt(DamageSource source, float amount) {
-        if (BIConfig.STALKER_IMMUNITY_ENABLED) {
-            if (source.getDirectEntity() instanceof AbstractArrow)
-                return false;
-            if (source.is(DamageTypes.FALL))
-                return false;
-            if (source.is(DamageTypes.DROWN))
-                return false;
-            if (source.is(DamageTypes.TRIDENT))
-                return false;
-            if (source.is(DamageTypes.WITHER))
-                return false;
-            if (source.is(DamageTypes.WITHER_SKULL))
-                return false;
-            else {
-                super.hurt(source, amount);
-                return true;
-            }
-        } else {
+    public boolean hurt(@NotNull DamageSource source, float amount) {
+        if (!BIConfig.STALKER_IMMUNITY_ENABLED) {
             super.hurt(source, amount);
             return true;
         }
+
+        if (source.getDirectEntity() instanceof AbstractArrow)
+            return false;
+        if (source.is(DamageTypes.FALL))
+            return false;
+        if (source.is(DamageTypes.DROWN))
+            return false;
+        if (source.is(DamageTypes.TRIDENT))
+            return false;
+        if (source.is(DamageTypes.WITHER))
+            return false;
+        if (source.is(DamageTypes.WITHER_SKULL))
+            return false;
+
+        super.hurt(source, amount);
+        return true;
     }
-
-
 
     @Override
     @Shadow
-    public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
-    }
+    public abstract void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar);
 
     @Override
     @Shadow
-    public AnimatableInstanceCache getAnimatableInstanceCache() {
-        return null;
-    }
+    public abstract AnimatableInstanceCache getAnimatableInstanceCache();
 }
